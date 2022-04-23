904fca7b925186df4e2883333509ef7b267a5fa0
Nathan Bisig <nathan.bisig@gmail.com>
2022-04-23 00:04:02 -0400

    remove clear
    remove storage clear

--- background.js
+++ background.js
@@ -5,15 +5,6 @@ const MAX_FILES = 30;
 var stopwords = ['i','also','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now','·','»','   '];
 // #endregion
 
-console.log("Background Started");
-chrome.storage.local.clear(
-    function() {
-        var error = chrome.runtime.lastError;
-        if (error) { console.log(error); }
-        else{ console.log("Local storage clear"); }
-    }
-);
-
 
 // #region Main
     // This method is the entry point to the background script.
